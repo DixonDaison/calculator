d4fde73fa73af7b3493c0ffa2cd3b1d5caa598d6
DixonDaison <dixondaison123@gmail.com>
2025-10-07 11:36:08 +0530

    feat: Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const display = document.getElementById('result');
+
+function clearScreen() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
